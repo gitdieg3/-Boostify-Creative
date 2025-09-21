06881ab57a3b9c288061ec117527b981bc818db0
Diegrss <136538662+gitdieg3@users.noreply.github.com>
2025-09-21 18:37:56 +0700

    Add files via upload

---
+++ class.py
@@ -0,0 +1,19 @@
+#void =  fungsi ygm tidak mengembalikan 
+#return = untuk mengembalikan data
+
+#def luas_persegi(sisi:int):
+#    return sisi * sisi
+#hasilpersegi = luas_persegi(4)
+#print("hasilpersegi adlahah:", hasilpersegi)
+
+
+# kombinasi perulangn dan function
+lisA = [1,2,3,4,5]
+
+def pangkat (x):
+    return x**10
+
+for i in lisA :
+    print (pangkat(i))
+
+#penjelasan nya deg ini seperti function di php , for i in ini unutk penglakgn
